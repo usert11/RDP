1801d1dd8941bfa3550b807bb7b6253b47e7347e
usert11 <76845413+usert11@users.noreply.github.com>
2021-01-01 17:40:43 +0100

    Update START.sh

--- START.sh
+++ START.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m USERT11
-sudo adduser USERT11 sudo
-echo 'USERT11:8426' | sudo chpasswd
+sudo useradd -m ALOK1
+sudo adduser ALOK1 sudo
+echo 'ALOK1:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser USERT11 chrome-remote-desktop
+sudo adduser ALOK1 chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - USERT11 -c """$CRP"""
+su - ALOK1 -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
